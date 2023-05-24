9c1b0c400cbc0de402b02081c0bc0d5caed8a328
Daniel Jacobo <jacoboitcg@gmail.com>
2023-05-23 22:00:49 -0600

    cambio staticfiles

--- ProyectoApeajal/settings.py
+++ ProyectoApeajal/settings.py
@@ -129,7 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Media root
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 MEDIA_URL="/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'ProyectoApeajal/static/csv')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'ProyectoApeajal/static/csv')
 
 
